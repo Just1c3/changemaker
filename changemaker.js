eaf52a9d54de33d917aa05264a4a80daf7f97dfe
kalen pecson <kalenpecson@students.berkeley.net>
2018-05-22 11:12:21 -0700

    e

--- changemaker.js
+++ changemaker.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $("#cloud").hide();
-    $("#enter").hide();
-    $("#mask").hide();
+    $(".hide").hide();
     $("#calculate").on("click",function(){
         $("#cloud").show();
         $("#cloud").animate({"height":"150%", "width":"150%","left":"-30%", "top":"-25%"},1000);
         $("#cloud").delay(400).fadeOut(1000);
         $("body").delay(400).css({"background-image":"url(https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Cumulus_Clouds_over_Yellow_Prairie2.jpg/1200px-Cumulus_Clouds_over_Yellow_Prairie2.jpg)","background-size":"100%"});
+        $("#search").show();
+        $(this).hide();
     });
     $("#signin").on("click", function(){
         showenters();
@@ -22,3 +22,5 @@ function showenters(){
     $("#mask").show();
     $("#enter").show();
 }
+
+var Listofkinds=["Mexican","Fast Food","Thai","Indian",];
