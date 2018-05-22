64b0d711b52216a9f27f7809642812938326881e
kalen pecson <kalenpecson@students.berkeley.net>
2018-05-22 11:36:44 -0700

    e

--- changemaker.js
+++ changemaker.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
         $("body").delay(400).css({"background-image":"url(https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Cumulus_Clouds_over_Yellow_Prairie2.jpg/1200px-Cumulus_Clouds_over_Yellow_Prairie2.jpg)","background-size":"100%"});
         $("#search").show();
         $(this).hide();
+        setupdropdowns();
     });
     $("#signin").on("click", function(){
         showenters();
@@ -23,4 +24,12 @@ function showenters(){
     $("#enter").show();
 }
 
-var Listofkinds=["Mexican","Fast Food","Thai","Indian",];
+function setupdropdowns(){
+    for(var i=0;i<Listofkinds.length;i++){
+        var name= Listofkinds[i];
+        $("#first").append("<div class='dropdown'><button class='dropbtn'>"+name+"</button><div class='dropdown-content' id='first'></div></div>")
+    }
+
+}
+
+var Listofkinds=["Mexican","Fast Food","Thai","Indian"];
